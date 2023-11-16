27f23b246df8b6d3b1956967b789881db8f7755c
Matt Svoboda <matt.svoboda@gmail.com>
2023-11-15 17:34:54 -0800

    Improve layout of Prefs Advanced view

--- iina/PlayerSaveState.swift
+++ iina/PlayerSaveState.swift
@@ -337,7 +337,7 @@ struct PlayerSaveState {
       player.$isShuttingDown.withLock() { isShuttingDown in
         guard !isShuttingDown else { return }
         let properties = generatePropDict(from: player)
-        player.log.verbose("Saving player state, tkt#\(saveTicket)")
+        player.log.verbose("Saving player state, tkt# \(saveTicket)")
 //        player.log.verbose("Saving player state: \(properties)")
         Preference.UIState.savePlayerState(forPlayerID: player.label, properties: properties)
       }
